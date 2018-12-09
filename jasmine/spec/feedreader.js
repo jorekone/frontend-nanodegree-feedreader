8b1b333787f1a6892cc7054cec673cb3df8f5a60
unknown <jorgeonuohacid@gmail.com>
2018-12-09 01:05:21 +0000

    All tests completed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -79,7 +79,9 @@ $(function() {
             expect(body. className).toBe('menu-hidden');
          });
     });
+    
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -87,11 +89,38 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('exist after calling loadFeed()', function(done) {
+            expect($('.feed')[0].getElementsByClassName('entry').length).toBeGreaterThan(0);
+            done();
+        }); 
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var feedText;
+        beforeEach(function(done) {
+            loadFeed(1, function() {
+                feedInnerText = $('.feed')[0].innerText;
+                loadFeed(2, function() {
+                    done();
+                });
+            });
+        });
+
+        it('content changes on new feed load', function(done) {
+            expect($('.feed')[0].innerText === feedInnerText).not.toBe(true);
+            done();
+        });
+    });
 }());
